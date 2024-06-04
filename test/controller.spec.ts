5672f55240266617734fa5d4ec702eb22e9be58b
holgerj4n <holgerjansohn@gmail.com>
2024-06-04 15:18:38 -0700

    Setup unit tests with jest

---
+++ test/controller.spec.ts
@@ -0,0 +1,48 @@
+import express from "express";
+import request from "supertest";
+import { Request, Response } from "express";
+import { Controller } from "./../src/controller"
+import { FileOps } from "./../src/file/file-ops.interface";
+
+describe('Controller', () => {
+    let testController: Controller;
+    let testApp: any;
+
+    const mockFileOps: FileOps = {
+        getFileNames: () => [],
+        getMostRecentEntries: () => []
+    }
+
+    beforeAll(() => {
+        testController = new Controller(mockFileOps);
+        testApp = express();
+        testApp.get('/files', (req: Request, res: Response) => {
+            testController.getFileNames(req, res);
+        });
+        testApp.get('/logs/:filename', (req: Request, res: Response) => {
+            testController.getLogs(req, res);
+        });
+    });
+
+    it('should return file names', async () => {
+        const expected = ["file1.txt", "file2.json"];
+        jest.spyOn(mockFileOps, 'getFileNames').mockReturnValueOnce(expected);
+        const response = await request(testApp).get('/files');
+
+        expect(response.status).toEqual(200);
+        expect(response.body).toEqual({ files: expected });
+    });
+
+    it('should return logs', async () => {
+        const expected = ["line1", "line2"];
+        jest.spyOn(mockFileOps, 'getMostRecentEntries').mockReturnValueOnce(expected);
+        const response = await request(testApp).get('/logs/messages.log');
+
+        expect(response.status).toEqual(200);
+        expect(response.body).toEqual({ logs: expected });
+    })
+
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+});
